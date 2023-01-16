9592a85a5de8a2348c9561bac281f0981818ab31
broklis777 <98511909+broklis777@users.noreply.github.com>
2023-01-16 14:36:59 +0200

    Šis tas no programmēšanas olimpiādes

---
+++ tests1.py
@@ -0,0 +1,6 @@
+a = 1
+b = 2 
+c = 3
+print(a, end=" ")
+print(b, end=" ")
+print(c, end=" ")
---
+++ tests2.py
@@ -0,0 +1,7 @@
+while True:
+    kkas = int(input("Ievade: "))
+    if str(kkas)[0] == "0":
+        print("AK VAI!!!!!")
+    else:
+        print("noice")
+        print(str(kkas)[0])
